b2bbadfd7dfcf7fa5acdb08384a67341ff3ade40
ps-cocjin <paulcocjin3@gmail.com>
2025-05-04 23:48:43 +0800

    added a js array for the checkout page, added a thank you, checkout, and fleshed out the clothing page to be functional

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const products = [
+    { id: 'p1', name: 'Graphic T-Shirt', price: 25, image: '../MIDTERM-PETA/images/product images/tshirt.png' },
+    { id: 'p2', name: 'Stylish Hoodie', price: 45, image: '../MIDTERM-PETA/images/product images/hoodie1.png' },
+    { id: 'p3', name: 'Modern Jacket', price: 60, image: '../MIDTERM-PETA/images/product images/jacket1.png' },
+    { id: 'p5', name: 'Casual Sneakers', price: 50, image: '../MIDTERM-PETA/images/product images/shoes.AVIF' },
+    { id: 'p4', name: 'Classic Cap', price: 120, image: '../MIDTERM-PETA/images/product images/cap1.png' },
+    { id: 'p6', name: 'Casual Tee', price: 30, image: '../MIDTERM-PETA/images/product images/tshirt2.png' },
+  ];
