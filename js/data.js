5e40bab3413588cf6a34352f0a74e70bea49c293
ps-cocjin <paulcocjin3@gmail.com>
2025-05-05 13:59:07 +0800

    added and updated to add a purchasing function

--- js/data.js
+++ js/data.js
@@ -5,4 +5,10 @@ const products = [
     { id: 'p5', name: 'Casual Sneakers', price: 50, image: '../MIDTERM-PETA/images/product images/shoes.AVIF' },
     { id: 'p4', name: 'Classic Cap', price: 120, image: '../MIDTERM-PETA/images/product images/cap1.png' },
     { id: 'p6', name: 'Casual Tee', price: 30, image: '../MIDTERM-PETA/images/product images/tshirt2.png' },
+    { id: 'section80', name: 'Section 80 Vinyl', price: 120, image: '../MIDTERM-PETA/images1/section.80vinyl.png' },
+    { id: 'gkmc', name: 'GKMC Vinyl', price: 120, image: '../MIDTERM-PETA/images1/gkmcvinyl.png' },
+    { id: 'tpab', name: 'To Pimp A Butterfly Vinyl', price: 120, image: '../MIDTERM-PETA/images1/tpabvinyl.png' },
+    { id: 'damn', name: 'DAMN. Vinyl', price: 120, image: '../MIDTERM-PETA/images1/damnvinyl.png' },
+    { id: 'mmatbs', name: 'Mr. Morale and The Big Steppers Vinyl', price: 120, image: '../MIDTERM-PETA/images1/mmatbsvinyl.png' },
+    { id: 'gnx', name: 'GNX Vinyl', price: 120, image: '../MIDTERM-PETA/images1/gnxvinyl.png' },
   ];
